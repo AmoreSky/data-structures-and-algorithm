12c58e273be66e4c19fd2d45434143d5cb8c3847
Adeoye Anthonia Oluwarantimi <toniaoluwarantimi@gmail.com>
2026-06-01 14:59:34 +0100

    Add initial implementation of Set container with basic operations

---
+++ setoriginal.js
@@ -0,0 +1,17 @@
+
+const container = new Set()
+
+container.add('lola')
+container.add('tola')
+container.add('fola')
+container.add('kola')
+
+console.log(container);
+console.log(container.size);
+console.log(container.has('mola'));
+console.log(container.delete('fola'));
+container.clear();
+console.log(container);
+
+
+
